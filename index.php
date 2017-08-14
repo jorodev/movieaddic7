edf80003dc6ed0181e8216deaf4a922080e62bf7
Georgi Georgiev <joro.dev@gmail.com>
2017-08-14 15:50:46 +0300

    added navigation and header in home page

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/css/reset.css">
+        <link rel="stylesheet" href="assets/css/index.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
+    </head>
+    <body>
+        <div class="nav-wrapper">
+            <nav class="nav container">
+                <div class="nav-logo">
+                    <h3><a href="login.php">Movie<span class="recolor">Addic7</span></a></h3>
+                </div>
+                <div class="nav-menu">
+                    <ul>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="home-header--wrapper">
+            <header class="home-header container">
+                <h1>Your movie tracker</h1>
+                <a href="#"><button>Join us now</button></a>
+            </header>
+        </div>
+    </body>
+</html>
