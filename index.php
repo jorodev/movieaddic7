bfdcfa1c02e72e9f48da3391ea13d32d51504810
Georgi Georgiev <joro.dev@gmail.com>
2017-08-15 13:20:59 +0300

    Added features section to main page

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
         <title></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
         <link rel="stylesheet" href="assets/css/reset.css">
         <link rel="stylesheet" href="assets/css/index.css">
-        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+ 
     </head>
     <body>
         <div class="nav-wrapper">
@@ -23,9 +25,33 @@
         </div>
         <div class="home-header--wrapper">
             <header class="home-header container">
-                <h1>Your movie tracker</h1>
+                <h1>Life is better with movies</h1>
                 <a href="#"><button>Join us now</button></a>
             </header>
+        </div>
+        <div class="home-features--wrapper">
+            <section class="home-features container">
+                <div class="home-features--watchlist">
+                    <i class="fa fa-plus fa-5x" aria-hidden="true"></i>
+                    <h6>Watchlist</h6><br />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore minus perferendis eos ullam quidem, aliquid alias consequatur obcaecati culpa corrupti tenetur molestiae recusandae.</p>
+                </div>
+                <div class="home-features--seenlist">
+                    <i class="fa fa-check fa-5x" aria-hidden="true"></i>
+                    <h6>SeenList</h6><br />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore minus perferendis eos ullam quidem, aliquid alias consequatur obcaecati culpa corrupti tenetur molestiae recusandae.</p>
+                </div>
+                <div class="home-features--favorites">
+                    <i class="fa fa-heart fa-5x" aria-hidden="true"></i>
+                    <h6>Favorites</h6><br />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore minus perferendis eos ullam quidem, aliquid alias consequatur obcaecati culpa corrupti tenetur molestiae recusandae.</p>
+                </div>
+                <div class="home-features--ratings">
+                    <i class="fa fa-star fa-5x" aria-hidden="true"></i>
+                    <h6>Ratings</h6><br />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore minus perferendis eos ullam quidem, aliquid alias consequatur obcaecati culpa corrupti tenetur molestiae recusandae.</p>
+                </div>
+            </section>
         </div>
     </body>
 </html>
