d7f7139660ab481bc894f4c4b891d6ba61dbfdbe
Georgi Georgiev <joro.dev@gmail.com>
2017-08-16 16:55:10 +0300

    Added content to the footer

--- index.php
+++ index.php
@@ -117,7 +117,9 @@
         </div>
         <div class="home-footer--wrapper">
             <footer class="home-footer container">
-                
+                &copy; 2017 | <a href="https://github.com/jorodev">Georgi Georgiev</a> - All rights reserved!<br /><br /><br />
+                <i class="fb fa fa-facebook fa-2x" aria-hidden="true"></i>
+                <i class="git fa fa-github fa-2x" aria-hidden="true"></i>
             </footer>
         </div>
 
